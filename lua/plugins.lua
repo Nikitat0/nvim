2dbede83985bc4987e019c7f9c3848d244361a3a
Nikitat0 <nikitatzero@yandex.ru>
2022-02-25 15:45:39 +0700

    Format

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,28 +1,28 @@
 vim.cmd [[packadd packer.nvim]]
 
-local use = require('packer').use
-require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
+local use = require("packer").use
+require("packer").startup(function()
+  use "wbthomason/packer.nvim"
 
-  use 'kyazdani42/nvim-web-devicons'
+  use "kyazdani42/nvim-web-devicons"
 
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/nvim-lsp-installer'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'L3MON4D3/LuaSnip'
+  use "neovim/nvim-lspconfig"
+  use "williamboman/nvim-lsp-installer"
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-cmdline"
+  use "L3MON4D3/LuaSnip"
 
-  use 'nvim-lualine/lualine.nvim'
-  use 'akinsho/bufferline.nvim'
+  use "nvim-lualine/lualine.nvim"
+  use "akinsho/bufferline.nvim"
 
-  use 'kyazdani42/nvim-tree.lua'
+  use "kyazdani42/nvim-tree.lua"
 
-  use 'powerman/vim-plugin-ruscmd'
+  use "powerman/vim-plugin-ruscmd"
 
-  use 'doums/darcula'
+  use "doums/darcula"
 
-  use 'andweeb/presence.nvim'
+  use "andweeb/presence.nvim"
 end)
--- lua/settings.lua
+++ lua/settings.lua
@@ -3,95 +3,97 @@ local exec = vim.api.nvim_exec
 local g = vim.g
 local opt = vim.opt
 
-opt.colorcolumn = '80'
+opt.colorcolumn = "80"
 opt.cursorline = true
 opt.number = true
 
 opt.termguicolors = true
-cmd'colorscheme darcula'
+cmd "colorscheme darcula"
 
 vim.o.timeoutlen = 250
 
-local lsp_installer = require("nvim-lsp-installer")
+local lsp_installer = require "nvim-lsp-installer"
 lsp_installer.on_server_ready(function(server)
-    server:setup({})
+  server:setup {}
 end)
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-vim.o.completeopt = 'menuone,noselect'
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+vim.o.completeopt = "menuone,noselect"
 
-local luasnip = require 'luasnip'
+local luasnip = require "luasnip"
 
-local cmp = require 'cmp'
+local cmp = require "cmp"
 cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "path" },
+    {
+      name = "buffer",
+      options = {
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
         end,
+      },
     },
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'path' },
-        { name = 'buffer', options = {
-            get_bufnrs = function()
-                return vim.api.nvim_list_bufs()
-            end
-    }},
-    },
+  },
 }
 
-require('presence'):setup({
-    auto_update         = true,
-    neovim_image_text   = "Neovim",
-    main_image          = "neovim",
-    client_id           = "793271441293967371",
-    log_level           = nil,
-    debounce_timeout    = 10,
-    enable_line_number  = false,
-    blacklist           = {},
-    buttons             = true,
-    file_assets         = {},
-    editing_text        = "Editing %s",
-    file_explorer_text  = "Browsing %s",
-    git_commit_text     = "Committing changes",
-    plugin_manager_text = "Managing plugins",
-    reading_text        = "Reading %s",
-    workspace_text      = "Working on %s",
-})
+require("presence"):setup {
+  auto_update = true,
+  neovim_image_text = "Neovim",
+  main_image = "neovim",
+  client_id = "793271441293967371",
+  log_level = nil,
+  debounce_timeout = 10,
+  enable_line_number = false,
+  blacklist = {},
+  buttons = true,
+  file_assets = {},
+  editing_text = "Editing %s",
+  file_explorer_text = "Browsing %s",
+  git_commit_text = "Committing changes",
+  plugin_manager_text = "Managing plugins",
+  reading_text = "Reading %s",
+  workspace_text = "Working on %s",
+}
 
-require'nvim-tree'.setup {
-      disable_netrw        = true,
-      open_on_setup        = false,
-      ignore_ft_on_setup   = {},
-      hijack_directories   = {
-        enable = true,
-        auto_open = true,
-      },
-      auto_close           = true,
-      auto_reload_on_write = true,
-      hijack_cursor        = true,
-      update_cwd           = true,
-      hijack_unnamed_buffer_when_opening = true,
-      diagnostics          = {
-        enable = true,
-        show_on_dirs = true,
-      },
-      trash = {
-        cmd = "trash",
-        require_confirm = true,
-      },
-      actions = {
-        change_dir = {
-          enable = true,
-        },
-        open_file = {
-	  quit_on_open = true,
-        },
-      },
-    }
+require("nvim-tree").setup {
+  disable_netrw = true,
+  open_on_setup = false,
+  ignore_ft_on_setup = {},
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
+  auto_close = true,
+  auto_reload_on_write = true,
+  hijack_cursor = true,
+  update_cwd = true,
+  hijack_unnamed_buffer_when_opening = true,
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+  actions = {
+    change_dir = {
+      enable = true,
+    },
+    open_file = {
+      quit_on_open = true,
+    },
+  },
+}
 
-require'bufferline'.setup{}
-require'lualine'.setup{}
-
+require("bufferline").setup {}
+require("lualine").setup {}
