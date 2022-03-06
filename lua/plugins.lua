0e7c0b21f3264f5980fc1fcdaf414a70409f2f90
Nikitat0 <nikitatzero@yandex.ru>
2022-03-06 13:44:04 +0700

    Sort by category

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,27 +2,29 @@ vim.cmd [[packadd packer.nvim]]
 
 local use = require("packer").use
 require("packer").startup(function()
+  --plugin management
   use "wbthomason/packer.nvim"
-
-  use "kyazdani42/nvim-web-devicons"
-
+  --lsp
   use "neovim/nvim-lspconfig"
   use "williamboman/nvim-lsp-installer"
+  --autocomplete
   use "hrsh7th/nvim-cmp"
   use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-buffer"
   use "hrsh7th/cmp-path"
   use "hrsh7th/cmp-cmdline"
-  use "L3MON4D3/LuaSnip"
-
+  --format
   use "mhartington/formatter.nvim"
-
-  use "nvim-lualine/lualine.nvim"
-  use "akinsho/bufferline.nvim"
-
+  --icons
+  use "kyazdani42/nvim-web-devicons"
+  --file explorer
   use "kyazdani42/nvim-tree.lua"
-
+  --bufferline
+  use "akinsho/bufferline.nvim"
+  --status
+  use "nvim-lualine/lualine.nvim"
+  --utils
   use "powerman/vim-plugin-ruscmd"
-
+  --theme
   use "doums/darcula"
 end)
--- lua/settings.lua
+++ lua/settings.lua
@@ -2,22 +2,14 @@ local cmd = vim.cmd
 local exec = vim.api.nvim_exec
 local g = vim.g
 local opt = vim.opt
-
-cmd(
-  [[
+--vim
+cmd [[
 filetype indent plugin on
 syntax enable
-augroup FormatAutogroup
-  autocmd!
-  autocmd BufWritePost * FormatWrite
-augroup END
-]],
-  true
-)
+]]
 
 opt.expandtab = true
 opt.shiftwidth = 4
-
 opt.tabstop = 4
 opt.smartindent = true
 
@@ -30,81 +22,13 @@ opt.so = 1000
 opt.splitright = true
 opt.splitbelow = true
 
-opt.termguicolors = true
-cmd "colorscheme darcula"
-
-vim.o.timeoutlen = 250
-
+opt.timeoutlen = 250
+--lsp
 local lsp_installer = require "nvim-lsp-installer"
 lsp_installer.on_server_ready(function(server)
   server:setup {}
 end)
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
-vim.o.completeopt = "menuone,noselect"
-
-local luasnip = require "luasnip"
-
-local cmp = require "cmp"
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "path" },
-    {
-      name = "buffer",
-      options = {
-        get_bufnrs = function()
-          return vim.api.nvim_list_bufs()
-        end,
-      },
-    },
-  },
-  mapping = {
-    ["<C-Space>"] = cmp.mapping.confirm { select = true },
-  },
-}
-
-require("nvim-tree").setup {
-  disable_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {},
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  auto_close = true,
-  auto_reload_on_write = true,
-  hijack_cursor = true,
-  update_cwd = true,
-  hijack_unnamed_buffer_when_opening = true,
-  diagnostics = {
-    enable = true,
-    show_on_dirs = true,
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
-  },
-  actions = {
-    change_dir = {
-      enable = true,
-    },
-    open_file = {
-      quit_on_open = true,
-    },
-  },
-}
-
-require("bufferline").setup {}
-require("lualine").setup {}
-
+--format
 require("formatter").setup {
   filetype = {
     lua = {
@@ -146,3 +70,48 @@ require("formatter").setup {
     },
   },
 }
+
+cmd [[
+augroup FormatAutogroup
+  autocmd!
+  autocmd BufWritePost * FormatWrite
+augroup END
+]]
+--file explorer
+require("nvim-tree").setup {
+  disable_netrw = true,
+  open_on_setup = false,
+  ignore_ft_on_setup = {},
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
+  auto_close = true,
+  auto_reload_on_write = true,
+  hijack_cursor = true,
+  update_cwd = true,
+  hijack_unnamed_buffer_when_opening = true,
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+  actions = {
+    change_dir = {
+      enable = true,
+    },
+    open_file = {
+      quit_on_open = true,
+    },
+  },
+}
+--bufferline
+require("bufferline").setup {}
+--status
+require("lualine").setup {}
+--theme
+opt.termguicolors = true
+cmd "colorscheme darcula"
