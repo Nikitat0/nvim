71324e40f4c6aeede970dd56c878bce5ae97c82e
Nikitat0 <nikitatzero@yandex.ru>
2022-05-15 15:31:30 +0700

    Add keymap.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,67 +1,76 @@
-local map = vim.api.nvim_set_keymap
-local default_opts = { noremap = true, silent = true }
+local map = require "config.keymap"
+
 --lsp
-map("n", "<F2>", ":lua vim.lsp.buf.rename()<CR>", default_opts)
-map("n", "<F7>", ":lua vim.lsp.buf.hover()<CR>", default_opts)
---snippets
-vim.cmd [[
-imap <expr> <CR> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<CR>'
-smap <expr> <CR> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<CR>'
+map { "<F2>", vim.lsp.buf.rename }
+map { "<F7>", vim.lsp.buf.hover }
 
-imap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'
-smap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'
-imap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'
-smap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'
-]]
---format
-map("n", "<F4>", ":FormatWrite<CR>", default_opts)
---file explorer
-map(
-  "n",
-  "<F6>",
-  ":NvimTreeRefresh<CR>:NvimTreeFindFileToggle<CR>",
-  default_opts
-)
 --find
-map("n", "<leader>f", ":Telescope find_files<CR>", default_opts)
-map("n", "<F3>", ":Telescope live_grep<CR>", default_opts)
+map { "<leader>f", "<Cmd>Telescope find_files<CR>" }
+map { "<F3>", "<Cmd>Telescope live_grep<CR>" }
 
-map("n", "<leader>lr", ":Telescope lsp_references<CR>", default_opts)
-map("n", "<leader>ls", ":Telescope lsp_document_symbols<CR>", default_opts)
-map("n", "<leader>lS", ":Telescope lsp_workspace_symbols<CR>", default_opts)
-map("n", "<leader>la", ":lua vim.lsp.buf.code_action()<CR>", default_opts)
-map("n", "<leader>lA", ":lua vim.lsp.buf.range_code_action()<CR>", default_opts)
-map("n", "<leader>l", ":Telescope diagnostics<CR>", default_opts)
-map("n", "<leader>li", ":Telescope lsp_implementations<CR>", default_opts)
-map("n", "<leader>ld", ":Telescope lsp_definitions<CR>", default_opts)
-map("n", "<leader>lt", ":Telescope lsp_type_definitions<CR>", default_opts)
+map { "<leader>lr", "<Cmd>Telescope lsp_references<CR>" }
+map { "<leader>ls", "<Cmd>Telescope lsp_document_symbols<CR>" }
+map { "<leader>lS", "<Cmd>Telescope lsp_workspace_symbols<CR>" }
+map { "<leader>la", vim.lsp.buf.code_action }
+map { "<leader>lA", vim.lsp.buf.range_code_action }
+map { "<leader>l", "<Cmd>Telescope diagnostics<CR>" }
+map { "<leader>li", "<Cmd>Telescope lsp_implementations<CR>" }
+map { "<leader>ld", "<Cmd>Telescope lsp_definitions<CR>" }
+map { "<leader>lt", "<Cmd>Telescope lsp_type_definitions<CR>" }
+--snippets
+for _, mode in ipairs { "s", "i" } do
+  map {
+    mode = mode,
+    "<CR>",
+    "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<CR>'",
+    expr = true,
+  }
+  map {
+    mode = mode,
+    "<Tab>",
+    "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'",
+    expr = true,
+  }
+  map {
+    mode = mode,
+    "<S-Tab>",
+    "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'",
+    expr = true,
+  }
+end
+--format
+map { "<F4>", "<Cmd>FormatWrite<CR>" }
+--file explorer
+map { "<F6>", "<Cmd>NvimTreeRefresh<CR><Cmd>NvimTreeFindFileToggle<CR>" }
 --buffers
-map("n", "<Tab>", ":Telescope buffers<CR>", default_opts)
-map("n", "<Tab>c", ":bdelete<CR>", default_opts)
-map("n", "<Tab>C", ":bdelete!<CR>", default_opts)
+map { "<Tab>", "<Cmd>Telescope buffers<CR>" }
+map { "<Tab>c", "<Cmd>bdelete<CR>" }
+map { "<Tab>C", "<Cmd>bdelete!<CR>" }
 --utils
-map("i", "jj", "<Esc>", { noremap = true })
-map("t", "jj", "<C-\\><C-n>", { noremap = true })
+map { mode = "i", "jj", "<Esc>" }
+map { mode = "t", "jj", "<C-\\><C-n>" }
 
-map("", "<up>", ':echoe "Use k"<CR>', { noremap = true, silent = false })
-map("", "<down>", ':echoe "Use j"<CR>', { noremap = true, silent = false })
-map("", "<left>", ':echoe "Use h"<CR>', { noremap = true, silent = false })
-map("", "<right>", ':echoe "Use l"<CR>', { noremap = true, silent = false })
+for _, mode in ipairs { "", "i" } do
+  map { mode = mode, "<up>", '<Cmd>echoe "Use k"<CR>', silent = false }
+  map { mode = mode, "<down>", '<Cmd>echoe "Use j"<CR>', silent = false }
+  map { mode = mode, "<left>", '<Cmd>echoe "Use h"<CR>', silent = false }
+  map { mode = mode, "<right>", '<Cmd>echoe "Use l"<CR>', silent = false }
+end
 
-map("n", "<F1>", ":h ", { noremap = false })
+map { "<F1>", ":h ", silent = false }
 
-map("n", "<leader>v", ":MarkdownPreview<CR>", default_opts)
+map { "<leader>v", "<Cmd>MarkdownPreview<CR>" }
 --git
-map("n", "<leader>[", ":Gitsigns prev_hunk<CR>", default_opts)
-map("n", "<leader>]", ":Gitsigns next_hunk<CR>", default_opts)
-map("n", "<leader>ga", ":Gitsigns stage_hunk<CR>", default_opts)
-map("n", "<leader>gA", ":Gitsigns stage_buffer<CR>", default_opts)
-map("n", "<leader>gu", ":Gitsigns undo_stage_hunk<CR>", default_opts)
-map("n", "<leader>gU", ":Gitsigns reset_buffer_index<CR>", default_opts)
-map("n", "<leader>gc", ":Git commit | only<CR>", default_opts)
-map("n", "<leader>gC", ":Git commit --amend | only<CR>", default_opts)
-map("n", "<leader>gp", ":Git push<CR>", default_opts)
-map("n", "<leader>gP", ":Git push --force<CR>", default_opts)
-map("n", "<leader>g", ":Git<CR>", default_opts)
-map("n", "<leader>gf", ":Telescope git_files<CR>", default_opts)
-map("n", "<leader>gb", ":Telescope git_branches<CR>", default_opts)
+map { "<leader>[", "<Cmd>Gitsigns prev_hunk<CR>" }
+map { "<leader>]", "<Cmd>Gitsigns next_hunk<CR>" }
+map { "<leader>ga", "<Cmd>Gitsigns stage_hunk<CR>" }
+map { "<leader>gA", "<Cmd>Gitsigns stage_buffer<CR>" }
+map { "<leader>gu", "<Cmd>Gitsigns undo_stage_hunk<CR>" }
+map { "<leader>gU", "<Cmd>Gitsigns reset_buffer_index<CR>" }
+map { "<leader>gc", "<Cmd>Git commit | only<CR>" }
+map { "<leader>gC", "<Cmd>Git commit --amend | only<CR>" }
+map { "<leader>gp", "<Cmd>Git push<CR>" }
+map { "<leader>gP", "<Cmd>Git push --force<CR>" }
+map { "<leader>g", "<Cmd>Git<CR>" }
+map { "<leader>gf", "<Cmd>Telescope git_files<CR>" }
+map { "<leader>gb", "<Cmd>Telescope git_branches<CR>" }
