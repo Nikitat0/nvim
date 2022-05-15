3f42c7782690d28f9c3f9ed27417605274fe640d
Nikitat0 <nikitatzero@yandex.ru>
2022-05-15 09:00:17 +0700

    Use new parameter name

--- lua/config/rust.lua
+++ lua/config/rust.lua
@@ -18,7 +18,7 @@ require("crates").setup {
       hide = { "q", "<esc>" },
       select = { "<cr>" },
       select_alt = { "s" },
-      copy_version = { "yy" },
+      copy_value = { "yy" },
       toggle_feature = { "<cr>" },
       goto_item = { "gd", "K" },
       jump_forward = { "<c-i>" },
