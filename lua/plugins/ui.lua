8a533ca7b0ee046c5555a19901f4e473ac1b0574
Nikitat0 <nikitatzero@yandex.ru>
2023-11-05 11:33:39 +0700

    Add telescope.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -28,5 +28,43 @@ return {
       require("lualine").setup()
     end,
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    name = "telescope",
+    dependencies = { "plenary" },
+    init = function()
+      local pickers = require "telescope.builtin"
+      keymap { "<F1>", pickers.help_tags }
+      keymap {
+        "<F3>",
+        function()
+          pickers.live_grep { grep_open_files = true }
+        end,
+      }
+      keymap { "<F3>", pickers.live_grep }
+      keymap { "<F4>", pickers.find_files }
+      keymap {
+        "<leader><F4>",
+        function()
+          pickers.find_files { hidden = true }
+        end,
+      }
+      keymap {
+        "<F11>",
+        function()
+          pickers.colorscheme { enable_preview = true }
+        end,
+      }
+      keymap { "<Tab><Tab>", pickers.buffers }
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    dependencies = { "telescope" },
+    init = function()
+      require("telescope").load_extension "ui-select"
+    end,
+  },
   "nvim-tree/nvim-web-devicons",
 }
