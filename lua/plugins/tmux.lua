419f7d7eb9cac42c07e66f94b73a0cd89ecec4be
Nikitat0 <nikitatzero@yandex.ru>
2023-11-07 14:10:09 +0700

    Add condition to enable integration with tmux

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -2,6 +2,9 @@ local keymap = require "keymap"
 
 return {
   "christoomey/vim-tmux-navigator",
+  cond = function()
+    return os.getenv "TMUX" ~= nil
+  end,
   init = function()
     vim.g.tmux_navigator_no_mappings = 1
     vim.g.tmux_navigator_save_on_switch = 1
