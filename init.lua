0cfc81e8a8c94b411c0562a70f879ccdcc41b0e2
Nikitat0 <nikitatzero@yandex.ru>
2023-11-07 10:04:42 +0700

    Add common keymaps

--- init.lua
+++ init.lua
@@ -11,4 +11,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup "plugins"
+require "keymaps"
---
+++ lua/keymaps.lua
@@ -0,0 +1,5 @@
+local keymap = require "keymap"
+
+keymap { "<Tab>q", "<Cmd>bdelete<CR>" }
+keymap { "<Tab>Q", "<Cmd>bdelete!<CR>" }
+keymap { "<F12>p", "<Cmd>Lazy<CR>" }
