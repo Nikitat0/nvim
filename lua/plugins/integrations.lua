079878602917060103656d6c2dbe560389951d76
Nikitat0 <nikitatzero@yandex.ru>
2023-11-07 14:49:07 +0700

    Merge specs of integration plugins

---
+++ lua/plugins/integrations.lua
@@ -0,0 +1,27 @@
+local keymap = require "keymap"
+
+return {
+  {
+    "christoomty/vim-tmux-navigator",
+    cond = function()
+      return os.getenv "TMUX" ~= nil
+    end,
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+      vim.g.tmux_navigator_save_on_switch = 1
+      keymap { "<A-h>", "<Cmd>TmuxNavigateLeft<CR>" }
+      keymap { "<A-j>", "<Cmd>TmuxNavigateDown<CR>" }
+      keymap { "<A-k>", "<Cmd>TmuxNavigateUp<CR>" }
+      keymap { "<A-l>", "<Cmd>TmuxNavigateRight<CR>" }
+    end,
+  },
+  {
+    "ivanesmantovich/xkbswitch.nvim",
+    cond = function()
+      return os.execute "xkb-switch" == 0
+    end,
+    init = function()
+      require "xkbswitch"
+    end,
+  },
+}
--- lua/plugins/xkb-switch.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "ivanesmantovich/xkbswitch.nvim",
-  cond = function()
-    return os.execute "xkb-switch" == 0
-  end,
-  init = function()
-    require "xkbswitch"
-  end,
-}
