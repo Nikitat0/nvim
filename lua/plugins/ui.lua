b5a5674c40809be2ecee07330abd322614941523
Nikitat0 <nikitatzero@yandex.ru>
2023-11-08 12:17:32 +0700

    Remove <F3> dead keymap

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -35,12 +35,6 @@ return {
     init = function()
       local pickers = require "telescope.builtin"
       keymap { "<F1>", pickers.help_tags }
-      keymap {
-        "<F3>",
-        function()
-          pickers.live_grep { grep_open_files = true }
-        end,
-      }
       keymap { "<F3>", pickers.live_grep }
       keymap { "<F4>", pickers.find_files }
       keymap {
