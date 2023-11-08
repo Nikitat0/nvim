ff43dd0239dc53c22e1084cc4c3cd38163f5ed81
Nikitat0 <nikitatzero@yandex.ru>
2023-11-08 22:36:34 +0700

    Revert "Use ssh instead of https in install.sh"
    This reverts commit 155138b5b78b2ffa326258686f14ee55f00db1b4.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ NVIM=$CONFIG_HOME/nvim
 NVIM_SWAP=$CONFIG_HOME/nvim.swp
 
 mkdir -p $NVIM
-git clone git@github.com:Nikitat0/nvim.git $NVIM_SWAP &&
+git clone https://github.com/Nikitat0/nvim.git $NVIM_SWAP &&
 git --work-tree=$NVIM --git-dir=$NVIM_SWAP/.git stash push --include-untracked
 rm -rf $NVIM
 mv $NVIM_SWAP $NVIM
