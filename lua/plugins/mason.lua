fd90343c308f003cfefb0dfb4e846dc544ce82ff
Nikitat0 <nikitatzero@yandex.ru>
2023-11-08 11:33:42 +0700

    Fix mason build function

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,7 +3,10 @@ local keymap = require "keymap"
 return {
   "williamboman/mason.nvim",
   name = "mason",
-  build = ":MasonUpdate",
+  build = function()
+    require("mason").setup()
+    vim.cmd "MasonUpdate"
+  end,
   init = function()
     require("mason").setup()
     keymap { "<F12>t", "<Cmd>Mason<CR>" }
