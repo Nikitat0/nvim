82792428553643f68aa25cb66b302d81da2336f8
Nikitat0 <nikitatzero@yandex.ru>
2023-11-08 10:29:26 +0700

    Fix install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ NVIM_SWAP=$CONFIG_HOME/nvim.swp
 mkdir -p $NVIM
 git clone https://github.com/Nikitat0/nvim.git $NVIM_SWAP &&
 git --work-tree=$NVIM --git-dir=$NVIM_SWAP/.git stash push --include-untracked
-rm $NVIM
+rm -rf $NVIM
 mv $NVIM_SWAP $NVIM
